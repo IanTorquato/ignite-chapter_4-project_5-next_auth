178a850542bf1b38c00e9f9c10da3cdee35d0002
Ian Torquato <iantorquato2@gmail.com>
2021-08-02 23:36:29 -0300

    Creating permissions hook

---
+++ next_auth-frontend/src/hooks/useCan.tsx
@@ -0,0 +1,34 @@
+import { useContext } from 'react';
+
+import { AuthContext } from '@nextauth/contexts/AuthContext';
+
+type UseCanParams = {
+  permissions?: string[];
+  roles?: string[];
+};
+
+export function useCan({ permissions, roles }: UseCanParams) {
+  const { user, isAuthenticated } = useContext(AuthContext);
+
+  if (!isAuthenticated) {
+    return false;
+  }
+
+  if (permissions?.length > 0) {
+    const hasAllPermissions = permissions.every((permission) => user.permissions.includes(permission));
+
+    if (!hasAllPermissions) {
+      return false;
+    }
+  }
+
+  if (roles?.length > 0) {
+    const hasAllRoles = roles.some((role) => user.roles.includes(role));
+
+    if (!hasAllRoles) {
+      return false;
+    }
+  }
+
+  return true;
+}
--- next_auth-frontend/src/pages/dashboard.tsx
+++ next_auth-frontend/src/pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from 'react';
 
 import { AuthContext } from '@nextauth/contexts/AuthContext';
+import { useCan } from '@nextauth/hooks/useCan';
 import { setupApiClient } from '@nextauth/services/api';
 import { api } from '@nextauth/services/apiClient';
 import { withSSRAuth } from '@nextauth/utils/withSSRAuth';
@@ -8,11 +9,18 @@ import { withSSRAuth } from '@nextauth/utils/withSSRAuth';
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
 
+  const userCanSeeMetrics = useCan({ permissions: ['metrics.list'] });
+
   useEffect(() => {
     api.get('/me').then(console.log).catch(console.error);
   }, []);
 
-  return <h1>Dashboard — {user?.email}</h1>;
+  return (
+    <>
+      <h1>Dashboard — {user?.email}</h1>
+      {userCanSeeMetrics && <h2>Metrics</h2>}
+    </>
+  );
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
